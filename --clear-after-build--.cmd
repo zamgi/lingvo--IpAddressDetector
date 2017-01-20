b57325cd1a15d415ebeaf2024a53f2d1647bbbea
zamgi <denisov72@mail.ru>
2017-01-20 13:41:17 +0300

    Add files via upload

---
+++ --clear-after-build--.cmd
@@ -0,0 +1,7 @@
+del "*.suo" /Q 2> nul
+del "ip.web.demo\bin\*.pdb" /Q 2> nul
+rd "ip.web.demo\obj" /S/Q 2> nul
+rd "IpAddressDetector\bin" /S/Q 2> nul
+rd "IpAddressDetector\obj" /S/Q 2> nul
+rd "IpAddressDetector.Console\bin" /S/Q 2> nul
+rd "IpAddressDetector.Console\obj" /S/Q 2> nul
