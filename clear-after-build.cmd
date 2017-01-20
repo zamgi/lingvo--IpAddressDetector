62f051ee7a666254e8d4fe24eb9b0b4d55dc2e6b
zamgi <zamgi@gmail.com>
2017-01-20 13:39:59 +0300

    ..

--- clear-after-build.cmd
+++
@@ -1,7 +0,0 @@
-del "*.suo" /Q 2> nul
-del "ip.web.demo\bin\*.pdb" /Q 2> nul
-rd "ip.web.demo\obj" /S/Q 2> nul
-rd "IpAddressDetector\bin" /S/Q 2> nul
-rd "IpAddressDetector\obj" /S/Q 2> nul
-rd "IpAddressDetector.Console\bin" /S/Q 2> nul
-rd "IpAddressDetector.Console\obj" /S/Q 2> nul
