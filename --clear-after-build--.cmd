8294b2fe69871b2bf7d4cbd9a7ba6dcc20b05f49
zamgi <denisov72@mail.ru>
2021-12-01 20:44:44 +0300

    Delete --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++
@@ -1,8 +0,0 @@
-del "*.suo" /S/Q/F/A
-del "*.csproj.user" /S/Q/F/A
-rd "ip.web.demo\bin" /S/Q
-rd "ip.web.demo\obj" /S/Q
-rd "IpAddressDetector\bin" /S/Q
-rd "IpAddressDetector\obj" /S/Q
-rd "IpAddressDetector.Console\bin" /S/Q
-rd "IpAddressDetector.Console\obj" /S/Q
